5514090c616b3c7740fe52000cc8a46c186c5a58
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-27 19:50:17 +0100

    abstracted header to components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const activities: string[] = [
   "listening to house music",
@@ -9,19 +10,29 @@ const activities: string[] = [
   "eating tortellini, kiwi & broccollini",
   "only pressing tab, Copilot <3",
   "navigating life with ADHD",
-  "caching my thoughts on paper",
-  "multitasking my way through life",
   "applying my creative imagination",
   "adding underlines to all my logos",
   "loving tailwindcss",
+  "multitasking my way through life",
   "hoping TypeScript will never leave me",
   "reading lots of Wikipedia articles",
   "enjoying the instant feedback of web dev",
   "sailing the seas",
 ];
 
+const variant = {
+  hidden: { opacity: 0, x: -0, y: -0 },
+  enter: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: { duration: 1 },
+  },
+};
+
 const Header = () => {
-  const [currentActivityIndex, setCurrentActivityIndex] = useState<number>(14);
+  const [currentActivityIndex, setCurrentActivityIndex] = useState<number>(7);
+  const [open, setOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -35,10 +46,10 @@ const Header = () => {
     return () => clearInterval(timer);
   }, [currentActivityIndex]);
   return (
-    <div className="flex flex-col items-center justify-center py-8  sm:pt-16 sm:pb-8">
+    <div className="flex flex-col items-center justify-center pt-12 pb-8 sm:pt-16 sm:pb-8">
       <h1
         className={`text-5xl font-semibold drop-shadow-lg ${
-          currentActivityIndex === 10 && "underline"
+          currentActivityIndex === 8 && "underline decoration-orange-500"
         }`}
       >
         Carl Lindman
@@ -51,23 +62,58 @@ const Header = () => {
           {activities[currentActivityIndex]}
         </span>
       </div>
-      <div className="mt-4 flex flex-row items-center justify-center gap-x-4 rounded-lg bg-slate-900 p-4 shadow-inner">
-        <a href="https://github.com/lindman-carl">
-          <Image
-            src="/github-mark.svg"
-            alt="github social"
-            width="32"
-            height="32"
-          />
-        </a>
-        <a href="https://linkedin.com/">
-          <Image
-            src="/linkedin-mark.svg"
-            alt="linkedin social"
-            width="32"
-            height="32"
-          />
-        </a>
+      <div className="flex w-full max-w-md flex-col items-center justify-center px-4">
+        <div className="relative top-8 z-20 mt-4 flex max-w-fit flex-row items-center justify-center gap-x-4 rounded-lg bg-orange-500 p-4 drop-shadow-lg">
+          <a href="https://github.com/lindman-carl">
+            <Image
+              src="/github-mark.svg"
+              alt="github social"
+              width="40"
+              height="40"
+              className="drop-shadow"
+            />
+          </a>
+          <a href="https://linkedin.com/">
+            <Image
+              src="/linkedin-mark.svg"
+              alt="linkedin social"
+              width="40"
+              height="40"
+              className="drop-shadow"
+            />
+          </a>
+        </div>
+        <motion.div
+          layout
+          className="z-10 flex h-full flex-col items-center justify-center rounded-lg bg-slate-900 p-4 px-8 text-justify shadow-inner"
+        >
+          <motion.div layout="position" className="mt-6 pb-4 text-sm">
+            I am a self-taught full stack web developer excited by building
+            complex stateful web apps with React.js heavily utilizing
+            TypeScript.
+          </motion.div>
+          {open && (
+            <motion.div layout="position" variants={variant}>
+              <div>
+                <h3>skills</h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+                repudiandae officia, aut iste, nemo quae nobis accusamus commodi
+                id dolores sunt asperiores dolorum libero voluptas voluptate
+                amet facilis saepe deleniti.
+              </div>
+            </motion.div>
+          )}
+        </motion.div>
+        {!open && (
+          <button
+            className=" relative bottom-6 z-20 w-full max-w-fit rounded-lg  bg-slate-900 py-2 px-4"
+            onClick={() => setOpen(!open)}
+          >
+            <span className="w-full text-lg font-bold text-white underline decoration-orange-500">
+              learn more
+            </span>
+          </button>
+        )}
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,69 +6,14 @@ const Home = () => {
   return (
     <Layout title="Portfolio" description="Carl Lindman's portfolio">
       <Header />
-      <div className="flex w-screen flex-col px-8 sm:px-24 lg:max-w-2xl lg:px-0">
-        <div className="h-full w-full ">
-          <h2 className="py-4 px-16 text-left text-xl font-bold sm:px-4">
-            skills
-          </h2>
-          <div className="grid grid-cols-3 gap-y-4 rounded-lg p-4 shadow-inner sm:grid-cols-7">
-            <div className="flex flex-col items-center justify-center gap-y-2 drop-shadow">
-              <Image
-                src="/skill-react.svg"
-                alt="react"
-                width="32"
-                height="32"
-              />
-              <span>React.js</span>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-y-2 drop-shadow">
-              <Image
-                src="/skill-js.svg"
-                alt="javascript"
-                width="32"
-                height="32"
-              />
-              <span>JavaScript</span>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-y-2 drop-shadow">
-              <Image
-                src="/skill-ts.svg"
-                alt="typescript"
-                width="32"
-                height="32"
-              />
-              <span>TypeScript</span>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-y-2 drop-shadow">
-              <Image src="/skill-html.svg" alt="html" width="32" height="32" />
-              <span>HTML</span>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-y-2 drop-shadow">
-              <Image src="/skill-css.svg" alt="css" width="32" height="32" />
-              <span>CSS</span>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-y-2 drop-shadow">
-              <Image src="/skill-svg.svg" alt="svg" width="32" height="32" />
-              <span>SVG</span>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-y-2 drop-shadow">
-              <Image
-                src="/skill-python.svg"
-                alt="python"
-                width="32"
-                height="32"
-              />
-              <span>Python</span>
-            </div>
-          </div>
-        </div>
+      <div className="flex w-screen flex-col overflow-x-hidden px-8 sm:px-24 lg:max-w-2xl lg:px-0">
         <h2 className="py-4 px-16 text-left text-xl font-bold sm:px-4">
           web apps
         </h2>
         <div className="grid w-full grid-flow-row grid-cols-1 gap-y-8 sm:grid-cols-2">
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center px-12 sm:px-0">
             <a
-              className="h-64 w-64 overflow-hidden rounded-xl shadow-xl"
+              className="h-full w-full overflow-hidden rounded-xl shadow-xl sm:h-64 sm:w-64"
               href="https://tackmantel.lindman.dev"
             >
               <Image
@@ -80,13 +25,13 @@ const Home = () => {
               />
             </a>
           </div>
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center px-12 sm:px-0">
             <a
-              className="h-64 w-64 overflow-hidden rounded-xl shadow-xl"
+              className="h-full w-full overflow-hidden rounded-xl shadow-xl sm:h-64 sm:w-64"
               href="https://seasweeper.lindman.dev"
             >
               <Image
-                src="/seasweeper-logo-960.png"
+                src="/seasweeper-logo-960.webp"
                 alt="sea sweeper web app"
                 width="960"
                 height="960"
